78257b574a2b75918243d6f8e6893b4405909fb6
sungwoo.yeo <yeosu0107@naver.com>
2022-12-12 14:55:18 +0900

    pbxproj 추가

---
+++ movieplex/movieplexApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieplexApp.swift
+//  movieplex
+//
+//  Created by sungwoo.yeo on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct movieplexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
